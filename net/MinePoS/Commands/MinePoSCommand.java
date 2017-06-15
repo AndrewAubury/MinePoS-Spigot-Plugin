07c8af96a2093f5add373a871ae4e447c02f880a
AndrewAubury <Nathan2010>
2017-06-15 19:52:55 +0100

    Added in the /MinePoS Command

--- net/MinePoS/Commands/MinePoSCommand.java
+++ net/MinePoS/Commands/MinePoSCommand.java
@@ -1,6 +1,7 @@
 package net.MinePoS.Commands;
 
 import net.MinePoS.Handlers.ChatHandler;
+import net.MinePoS.Main;
 import net.MinePoS.Messages;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,6 +17,18 @@ if(label.equalsIgnoreCase("minepos")){
     ChatHandler chat = new ChatHandler();
     if(sender.hasPermission("minepos.admin")){
         if(args.length == 0){
+            chat.sendHelp(sender);
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("poll")) {
+                chat.sendString(sender, "&cServer will now update Orders");
+                Main.getInstance().API.getWaiting();
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                chat.sendString(sender, "&cConfig file reloaded");
+                Main.getInstance().reloadConfig();
+            } else {
+                chat.sendHelp(sender);
+            }
+        } else {
             chat.sendHelp(sender);
         }
     }else{
